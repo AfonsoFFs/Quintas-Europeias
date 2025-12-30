c5afab853dc51f24c7fbcbb5a51944795d13a1e8
Afonso Fernandes <afonso.ffs@gmail.com>
2025-12-30 10:55:05 +0000

    move index

--- vite.config.ts
+++ vite.config.ts
@@ -3,9 +3,7 @@ import react from "@vitejs/plugin-react-swc";
 import path from "path";
 import { componentTagger } from "lovable-tagger";
 
-// https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
-  root: "src", 
   server: {
     host: "::",
     port: 8080,
@@ -17,7 +15,7 @@ export default defineConfig(({ mode }) => ({
     },
   },
   build: {
-    outDir: "../dist", 
+    outDir: "dist",  
     emptyOutDir: true,
   },
 }));
